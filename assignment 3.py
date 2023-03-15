3013151deff8e11a0d90472aa193da308ae63239
Tiffany Pureheart <126727956+Greatestguru007@users.noreply.github.com>
2023-03-15 11:16:59 +0100

    Python +ve, -ve and zero

---
+++ assignment 3.py
@@ -0,0 +1,7 @@
+digit = float(input("Enter a number: "))
+if digit > 0:
+    print("The number is positive.")
+elif digit < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
